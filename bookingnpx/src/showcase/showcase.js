52ef65df4726ade1cca036d5bc414ee2b1b57b02
AlbertSamuelMelo <asm.asm.albert@icloud.com>
2020-07-27 06:35:38 -0400

    created showcase carousel
    Signed-off-by: AlbertSamuelMelo <asm.asm.albert@icloud.com>

--- bookingnpx/src/showcase/showcase.js
+++ bookingnpx/src/showcase/showcase.js
@@ -1,22 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+import ItemsCarousel from 'react-items-carousel';
+import styled from 'styled-components';
+
+const chevronWidth = 40;
+
+const Wrapper = styled.div`
+  padding: 0 ${chevronWidth}px;
+  margin: 0 auto;
+`;
+
+const SlideItem = styled.div`
+  height: 200px;
+  width: 160px;
+  background: #EEE;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  font-weight: bold;
+`;
 
 class Showcase extends Component {
 
   constructor(props){
     super(props);
+    this.state = {
+        activeItemIndex: 0
+      };
   }
 
   render() {
     return (
-      <div style={style.showcase}>
-        <text>{String(this.props.data.immutableId)}</text>
-      </div>
+        <Wrapper>
+            <ItemsCarousel
+                requestToChangeActive={value => this.setState({ activeItemIndex: value })}
+                activeItemIndex={this.state.activeItemIndex}
+                leftChevron={'<'}
+                rightChevron={'>'}
+                infiniteLoop={false}
+                gutter={15}
+                activePosition={'center'}
+                chevronWidth={60}
+                disableSwipe={false}
+                alwaysShowChevrons={false}
+                numberOfCards={9}
+                slidesToScroll={3}
+                outsideChevron={true}
+                showSlither={true}
+                firstAndLastGutter={true}
+                >
+                    {Array.from(new Array(18)).map((_, i) =>
+                    <SlideItem key={i}>
+                        {i+1}
+                    </SlideItem>
+                    )}
+            </ItemsCarousel>
+        </Wrapper>
     );
   }
 }
-const style = {
-    showcase:{
-        margin: 15,
-    }
-};
 export default Showcase;
