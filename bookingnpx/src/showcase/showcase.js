128b588a9de54582b629a7da456f54fa2df6213e
AlbertSamuelMelo <asm.asm.albert@icloud.com>
2020-07-28 16:34:16 -0400

    card style
    Signed-off-by: AlbertSamuelMelo <asm.asm.albert@icloud.com>

--- bookingnpx/src/showcase/showcase.js
+++ bookingnpx/src/showcase/showcase.js
@@ -15,11 +15,47 @@ const SlideItem = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  font-size: 13px;
-  font-weight: bold;
   flex-direction: column;
+  font-size: 15px;
+  font-family: "Amazon Ember Arial sans-serif";  
+  text-align: left;
 `;
 
+const TitleText = styled.text`
+  color: #0066c0!important;
+  font-size: 13px!important;
+  word-break: normal;
+  line-height: 1.3em !important;
+  width: 100%;
+  margin: 3px`;
+
+const InstallmentText = styled.text`
+  font-size: 13px!important;
+  color: #555;
+  line-height: 1.3em !important;
+  width: 100%;
+`;
+
+const CurrencyPrice = styled.text`
+  list-style: decimal;
+  color: #111;
+  font-size: 1.2em;
+  font-weight: semi-bold
+  `;
+
+const CurrencyText = styled.text`
+  text-align: left;
+  color: #111;
+  position: relative;
+  top: -.5em;
+  font-size: 10px;`;
+
+  const SellerText = styled.text`
+    color: #555;
+    font-size: 11px;
+    line-height: 1.25;
+    width: 100%;
+  `;
 class Showcase extends Component {
 
   constructor(props){
@@ -66,14 +102,28 @@ class Showcase extends Component {
                 >
                     {Array.from(Object.entries(this.props.data.products)).map((entri, i) =>
                     <SlideItem key={i}>
-                        <img src={entri[1].image.extraLarge != null ? entri[1].image.extraLarge : entri[1].image.large} 
-                             alt={entri[1].image.name + "Image"}
-                            style={{width: '100%'}}></img>
-                        {entri[1].name}
-                        <text>{entri[1].installment}</text>
-                        <text>{entri[1].offerPrice + " " + entri[1].currency}</text>
-                        <text>{entri[1].sellerName}</text>
-                        
+                      <div style={{top: '0px !important', height: '50%'}}>
+                        <img src={entri[1].image.extraLarge != null ? entri[1].image.extraLarge : entri[1].image.large}
+                          alt={entri[1].image.name + "Image"}
+                          style={{width: '100%'}}></img>
+                      </div>
+                      <div style={{
+                        margin: 5, 
+                        height: '50%', 
+                        bottom: 5,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'flex-start',
+                        }}>
+                        <TitleText>{entri[1].name}</TitleText>
+                        <InstallmentText>{entri[1].installment}</InstallmentText>
+                        <div style={{margin: '8px', textAlign: 'center'}}> 
+                          <CurrencyPrice>{entri[1].offerPrice}</CurrencyPrice>
+                          <CurrencyText>{entri[1].currency}</CurrencyText>
+                        </div>
+                        <SellerText>{entri[1].sellerName}</SellerText>
+                      </div>
                     </SlideItem>
                     )}
             </ItemsCarousel>
